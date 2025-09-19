28678b086bfc8be97c5639a081a1e5d7f8abf815
Enrico44444 <escarpeggiani@ucs.br>
2025-09-19 18:45:00 -0300

    feito a página de roteiros

---
+++ roteirosdados.js
@@ -0,0 +1,107 @@
+// Variáveis dos roteiros - para preenchimento posterior
+
+// Roteiro 1 - Alfa
+let roteiro1 = `
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque id omnis, reiciendis, ab saepe tempora distinctio harum, beatae nam pariatur dignissimos. Temporibus ex tempora corrupti repudiandae eius ipsa quasi explicabo.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non est incidunt itaque, totam repudiandae rerum commodi dolorum dolorem voluptatem reiciendis vero cumque aspernatur similique. Aut praesentium rem vitae? Rerum, culpa!
+
+
+`;
+
+// Roteiro 2 - Beta
+let roteiro2 = `
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque id omnis, reiciendis, ab saepe tempora distinctio harum, beatae nam pariatur dignissimos. Temporibus ex tempora corrupti repudiandae eius ipsa quasi explicabo.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non est incidunt itaque, totam repudiandae rerum commodi dolorum dolorem voluptatem reiciendis vero cumque aspernatur similique. Aut praesentium rem vitae? Rerum, culpa!
+
+
+`;
+
+// Roteiro 3 - Delta
+let roteiro3 = `
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque id omnis, reiciendis, ab saepe tempora distinctio harum, beatae nam pariatur dignissimos. Temporibus ex tempora corrupti repudiandae eius ipsa quasi explicabo.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non est incidunt itaque, totam repudiandae rerum commodi dolorum dolorem voluptatem reiciendis vero cumque aspernatur similique. Aut praesentium rem vitae? Rerum, culpa!
+
+
+`;
+
+// Roteiro 4 - Gama
+let roteiro4 = `
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque id omnis, reiciendis, ab saepe tempora distinctio harum, beatae nam pariatur dignissimos. Temporibus ex tempora corrupti repudiandae eius ipsa quasi explicabo.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non est incidunt itaque, totam repudiandae rerum commodi dolorum dolorem voluptatem reiciendis vero cumque aspernatur similique. Aut praesentium rem vitae? Rerum, culpa!
+
+
+`;
+
+// Roteiro 5 - Kappa
+let roteiro5 = `
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque id omnis, reiciendis, ab saepe tempora distinctio harum, beatae nam pariatur dignissimos. Temporibus ex tempora corrupti repudiandae eius ipsa quasi explicabo.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non est incidunt itaque, totam repudiandae rerum commodi dolorum dolorem voluptatem reiciendis vero cumque aspernatur similique. Aut praesentium rem vitae? Rerum, culpa!
+
+
+`;
+
+// Roteiro 6 - Zeta
+let roteiro6 = `
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque id omnis, reiciendis, ab saepe tempora distinctio harum, beatae nam pariatur dignissimos. Temporibus ex tempora corrupti repudiandae eius ipsa quasi explicabo.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non est incidunt itaque, totam repudiandae rerum commodi dolorum dolorem voluptatem reiciendis vero cumque aspernatur similique. Aut praesentium rem vitae? Rerum, culpa!
+
+
+`;
+
+// Objeto com dados dos roteiros
+const roteirosData = {
+  'alfa': {
+    titulo: 'Título',
+    icon: '🚀',
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.',
+    conteudo: roteiro1
+  },
+  'beta': {
+    titulo: 'Título',
+    icon: '🏰',
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.',
+    conteudo: roteiro2
+  },
+  'delta': {
+    titulo: 'Título',
+    icon: '🌃',
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.',
+    conteudo: roteiro3
+  },
+  'gama': {
+    titulo: 'Título',
+    icon: '🌲',
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.',
+    conteudo: roteiro4
+  },
+  'kappa': {
+    titulo: 'Título',
+    icon: '🧟',
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.',
+    conteudo: roteiro5
+  },
+  'zeta': {
+    titulo: 'Título',
+    icon: '🏴‍☠️',
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur ipsam ipsa culpa expedita architecto fugiat earum temporibus suscipit est asperiores numquam excepturi, nobis quasi deleniti ex! Laboriosam, deleniti sit.',
+    conteudo: roteiro6
+  }
+}
